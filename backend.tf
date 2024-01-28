ed5ab1856c26c31d8f04bf93dcca8f924eb9efe6
Evelyn Johannsen <evelyn-johannsen@outlook.de>
2024-01-28 22:33:22 +0100

    backend

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "jenkins-bucket-Evelyn-Homework"
+    key            = "state/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "dynamodb-terraform-lock"
+    encrypt        = true
+  }
+}
